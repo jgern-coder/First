6d4bee3daa6c039ae9af6e05e45ff21c457ad33e
jgern-coder <jeff.gernert@angel.com>
2026-03-09 04:50:36 -0400

    Add files via upload

---
+++ src/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BoxOfficeProjector from './BoxOfficeProjector.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BoxOfficeProjector />
+  </React.StrictMode>
+)
